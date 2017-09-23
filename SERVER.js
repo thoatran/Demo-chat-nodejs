576ddf98ebfc7fd341b5e96346f7e841c76fc397
thoatran <superjunior13wook@gmail.com>
2017-09-23 20:21:18 +0700

    Initial commit

---
+++ SERVER.js
@@ -0,0 +1,52 @@
+var express = require("express");
+var app = express();
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
+server.listen(3000);
+
+var mangUsers=[];
+
+io.on("connection", function(socket){
+  console.log("Connect: " + socket.id);
+
+  socket.on("client-send-Username", function(data){
+    if(mangUsers.indexOf(data)>=0){
+      socket.emit("server-send-failed-register");
+    }else{
+      mangUsers.push(data);
+      socket.Username = data;
+      socket.emit("server-send-successful-register", data);
+      io.sockets.emit("server-send-list-users", mangUsers);
+    }
+  });
+
+  socket.on("logout", function(){
+    mangUsers.splice(
+      mangUsers.indexOf(socket.Username), 1
+    );
+    socket.broadcast.emit("server-send-list-users",mangUsers);
+  });
+
+  socket.on("user-send-message", function(data){
+    io.sockets.emit("server-send-mesage", {un:socket.Username, nd:data} );
+  });
+
+  socket.on("I-am-typing", function(){
+    var s = socket.Username + " dang go chu";
+    io.sockets.emit("typing", s);
+  });
+
+  socket.on("I-stop-typing", function(){
+    io.sockets.emit("STOP-typing");
+  });
+
+
+});
+
+app.get("/", function(req, res){
+  res.render("trangchu");
+});
---
+++ public/xuly.js
@@ -0,0 +1,60 @@
+var socket = io("http://localhost:3000");
+
+socket.on("server-send-failed-register", function(){
+  alert("Sai Username (co nguoi da dang ki roi!!!)");
+});
+
+socket.on("server-send-list-users", function(data){
+  $("#boxContent").html("");
+  data.forEach(function(i){
+    $("#boxContent").append("<div class='user'>" + i + "</div>");
+  });
+});
+
+socket.on("server-send-successful-register", function(data){
+  $("#currentUser").html(data);
+  $("#loginForm").hide(2000);
+  $("#chatForm").show(1000);
+});
+
+socket.on("server-send-mesage", function(data){
+  $("#listMessages").append("<div class='ms'>" + data.un + ":" + data.nd +"</div>");
+});
+
+socket.on("typing", function(data){
+  $("#thongbao").html("<img width='20px' src='typing05.gif'> " + data);
+});
+
+socket.on("STOP-typing", function(){
+  $("#thongbao").html("");
+});
+
+
+$(document).ready(function(){
+  $("#loginForm").show();
+  $("#chatForm").hide();
+
+  $("#txtMessage").focusin(function(){
+    socket.emit("I-am-typing");
+  })
+
+  $("#txtMessage").focusout(function(){
+    socket.emit("I-stop-typing");
+  })
+
+  $("#btnRegister").click(function(){
+    socket.emit("client-send-Username", $("#txtUsername").val());
+  });
+
+  $("#btnLogout").click(function(){
+    socket.emit("logout");
+    $("#chatForm").hide(2000);
+    $("#loginForm").show(1000);
+  });
+
+  $("#btnSendMessage").click(function(){
+    socket.emit("user-send-message", $("#txtMessage").val());
+  });
+
+
+});
